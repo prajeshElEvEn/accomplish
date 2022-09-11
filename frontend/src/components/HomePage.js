ec78cd3f520bb8e253fa2aa89109d57a5794ded0
prajeshElEvEn <prajesh.eleven118@gmail.com>
2022-09-11 20:45:00 +0530

    - created components - created styles

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div classname='container'>
+            <div className='logo'>
+                Accomplish
+            </div>
+            <div className='title-content'>
+                <div className='home-pre-title'>
+                    Assign. Manage. <span className='title-logo'>Accomplish</span>
+                </div>
+                <div className='home-title'></div>
+                <div className='home-sub-title'></div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
